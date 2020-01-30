a79965a9ea6fe6314222ae5a340856f5db53734b
dzivenu <makafuigdzivenu@gmail.com>
2020-01-30 13:53:56 +0000

    export function

--- src/lib/utils/sys.js
+++ src/lib/utils/sys.js
@@ -1,7 +1,7 @@
 
 import { maxLogs } from "@constants"
 
-const insertActivity = (activities, newAct) => {
+export const insertActivity = (activities, newAct) => {
   activities.unshift(newAct)
   return activities.length < maxLogs ? activities : activities.slice(0, maxLogs)
 }
